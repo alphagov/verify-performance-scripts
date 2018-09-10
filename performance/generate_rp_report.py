e3f25270c467bfb2c44d962c77044bf2eef1dad0
Paula Valenca <paula.valenca@digital.cabinet-office.gov.uk>
2018-09-10 14:58:09 +0100

    PMI587 - Refactor MVP RP report script to run() function
    Co-Authored-By: Donna Belsey <donna.belsey@digital.cabinet-office.gov.uk>

--- performance/generate_rp_report.py
+++ performance/generate_rp_report.py
@@ -14,45 +14,17 @@ import os
 from datetime import date, timedelta
 
 VERIFY_DATA_PIPELINE_CONFIG_PATH = '../../verify-data-pipeline-config'
-
-
-def load_args_from_command_line():
-    parser = argparse.ArgumentParser(description=__doc__)
-
-    parser.add_argument('--report_start_date', help='expected format: YYYY-MM-DD', required=True)
-    parser.add_argument('--report_output_path', help='relative path to output report CSV', default='../output')
-
-    args = parser.parse_args()
-    return args
-
-
-params = load_args_from_command_line()
-
-period = 'week'
-limit = '-1'
-date_start = date.fromisoformat(params.report_start_date)
-date_end = date_start + timedelta(days=6)
-date_start_string = params.report_start_date
-verifications_by_rp_csv = \
-    f'{VERIFY_DATA_PIPELINE_CONFIG_PATH}/data/verifications/verifications_by_rp_{date_start}_{date_end}.csv'
-report_output_path = params.report_output_path
+BASE_URL = 'https://analytics-hub-prod-a-dmz.ida.digital.cabinet-office.gov.uk/index.php'
 ENV = 'prod'
 
-if ENV == 'prod':
-    BASE_URL = 'https://analytics-hub-prod-a-dmz.ida.digital.cabinet-office.gov.uk/index.php'
-else:
-    BASE_URL = 'https://analytics-hub-dr-dmz.ida.digital.cabinet-office.gov.uk/index.php'
+# TODO this should come from config
+LOA1_RP_LIST = ["DFT DVLA VDL", "Get your State Pension", "NHS TRS"]
 
 
 def get_piwik_token(env):
     with open(f'{VERIFY_DATA_PIPELINE_CONFIG_PATH}/credentials/piwik_token.json') as fileHandle:
         token = json.load(fileHandle)['production' if env == 'prod' else 'dr_token']
         return token
-
-
-token = get_piwik_token(ENV)
-
-df_verifications_by_rp = pd.read_csv(verifications_by_rp_csv)
 
 
 def get_nb_visits_for_page(date, period, token, limit, segment):
@@ -75,10 +47,6 @@ def get_nb_visits_for_page(date, period, token, limit, segment):
     return nb_visits
 
 
-# ## Getting the volume of sessions for sign up and sign in
-# No longer uses unique page views - gets the number of sessions for each value of the JOURNEY_TYPE custom variable
-
-
 def get_nb_visits_for_rp(date, period, token, limit, segment):
     qs = {
         'module': 'API',
@@ -99,115 +67,114 @@ def get_nb_visits_for_rp(date, period, token, limit, segment):
     return raw_result.get('value', 0)
 
 
-# rp_mapping translates the referrer url reported in the verifications csv
-with open(f'{VERIFY_DATA_PIPELINE_CONFIG_PATH}/configuration/rp_mapping.json') as ft:
-    rp_mapping = json.load(ft)
-
-
-def get_rp_name(rp_entity_id):
-    return rp_mapping[rp_entity_id]
-
-
-# Get the human readable IDP name and add it to a new column
-
-
-df_verifications_by_rp['rp'] = df_verifications_by_rp.apply(lambda row: get_rp_name(row['RP Entity Id']), axis=1)
-df_verifications_by_rp.head()
-
-df_verifications_by_rp = df_verifications_by_rp.rename(columns={'Response type': 'response_type'})
-
-df_totals = df_verifications_by_rp.groupby(['rp', 'response_type']).count().reset_index()
-df_totals.head()
-
-df_totals.drop(['Timestamp', 'RP Entity Id'], axis=1, inplace=True)
-
-df_totals = df_totals.rename(columns={'IDP Entity Id': 'successes'})
-
-df_all = pd.pivot_table(df_totals, values='successes', index='rp', columns='response_type')
-df_all.reset_index(inplace=True)
-df_all.columns = ['rp', 'signup_success', 'signin_success']
-
-# ### Success rate (sign in)
-
-
-ls_rp = list(df_verifications_by_rp.rp.unique())
-
-df = pd.DataFrame()
-
-for rp in ls_rp:
-    print("Getting data for {}".format(rp))
-    segment_by_rp = f"customVariableValue1=={rp}"
-    all_referrals = get_nb_visits_for_rp(date_start_string, period, token, limit, segment_by_rp)
-    segment_signin = f"{segment_by_rp};customVariableValue3==SIGN_IN"
-    signin_attempts = get_nb_visits_for_rp(date_start_string, period, token, limit, segment_signin)
-    segment_signup = f"{segment_by_rp};customVariableValue3==REGISTRATION"
-    signup_attempts = get_nb_visits_for_rp(date_start_string, period, token, limit, segment_signup)
-
-    segment_single_idp = f"{segment_by_rp};customVariableValue3==SINGLE_IDP"
-    single_idp_attempts = get_nb_visits_for_rp(date_start_string, period, token, limit, segment_single_idp)
-
-    # TODO if the RP is not found (e.g. due to no successful signins) then we may need to add a row?
-    df_all.loc[(df_all['rp'] == rp), 'all_referrals'] = all_referrals
-    df_all.loc[(df_all['rp'] == rp), 'signin_attempt'] = signin_attempts
-    df_all.loc[(df_all['rp'] == rp), 'signup_attempt'] = signup_attempts
-    df_all.loc[(df_all['rp'] == rp), 'single_idp_attempt'] = single_idp_attempts
-
-df_all['signin_rate (deprecated)'] = df_all['signin_success'] / df_all['signin_attempt']
-df_all['signup_rate (deprecated)'] = df_all['signup_success'] / df_all['signup_attempt']
-
-df_all['all_referrals_with_intent'] = df_all['signin_attempt'] + df_all['signup_attempt'] + df_all['single_idp_attempt']
-
-df_all['success'] = df_all['signin_success'] + df_all['signup_success']
-df_all['success_rate'] = df_all['success'] / df_all['all_referrals_with_intent']
-
-df_all['success_fraction_signup'] = df_all['signup_success'] / df_all['success']
-df_all['success_fraction_signin'] = df_all['signin_success'] / df_all['success']
-
-df_all['success_signup_rate'] = df_all['signup_success'] / df_all['all_referrals_with_intent']
-df_all['success_signin_rate'] = df_all['signin_success'] / df_all['all_referrals_with_intent']
-
-df_all['dropout'] = df_all['all_referrals_with_intent'] - df_all['signin_success'] - df_all['signup_success']
-df_all['dropout_rate'] = df_all['dropout'] / df_all['all_referrals_with_intent']
-
-# ### Ineligible (will not work / might not work) - with journey type segment
-# upvson 'GOV.UK Verify will not work for you' page, segmented by 'registration' custom variable
-
-# TODO this should come from config
-loa1_rp_list = ["DFT DVLA VDL", "Get your State Pension", "NHS TRS"]
-
-
 def is_loa2(rp):
-    return rp not in loa1_rp_list
+    return rp not in LOA1_RP_LIST
 
 
-for rp in ls_rp:
-    if is_loa2(rp):
-        will_not_work_page = "@GOV.UK Verify will not work for you - GOV.UK Verify - GOV.UK - LEVEL_2"
-        will_not_work_segment = "pageTitle={};customVariableValue1=={};customVariableValue3==REGISTRATION".format(
-            will_not_work_page, rp)
-        visits_will_not_work = get_nb_visits_for_page(date_start_string, period, token, limit, will_not_work_segment)
-        might_not_work_page = "@Why might this not work for me - GOV.UK Verify - GOV.UK - LEVEL_2"
-        might_not_work_segment = "pageTitle={};customVariableValue1=={};customVariableValue3==REGISTRATION".format(
-            might_not_work_page, rp)
-        visits_might_not_work = get_nb_visits_for_page(date_start_string, period, token, limit, might_not_work_segment)
-        row = df_all.loc[(df_all['rp'] == rp), 'visits_will_not_work'] = visits_will_not_work
-        row = df_all.loc[(df_all['rp'] == rp), 'visits_might_not_work'] = visits_might_not_work
+def load_args_from_command_line():
+    parser = argparse.ArgumentParser(description=__doc__)
 
-# ## Export results
+    parser.add_argument('--report_start_date', help='expected format: YYYY-MM-DD', required=True)
+    parser.add_argument('--report_output_path', help='relative path to output report CSV', default='../output')
+
+    args = parser.parse_args()
+    return args
 
 
-# Re-order columns and choose the ones we actually (currently) want in our report
-df_export = df_all[['rp', 'all_referrals_with_intent', 'success', 'success_fraction_signup', 'success_fraction_signin',
-                    'visits_will_not_work', 'visits_might_not_work']]
+def run():
+    params = load_args_from_command_line()
+    period = 'week'
+    limit = '-1'
+    date_start = date.fromisoformat(params.report_start_date)
+    date_end = date_start + timedelta(days=6)
+    date_start_string = params.report_start_date
+    verifications_by_rp_csv = \
+        f'{VERIFY_DATA_PIPELINE_CONFIG_PATH}/data/verifications/verifications_by_rp_{date_start}_{date_end}.csv'
+    report_output_path = params.report_output_path
 
-if not os.path.exists(report_output_path):
-    os.makedirs(report_output_path)
+    token = get_piwik_token(ENV)
+    df_verifications_by_rp = pd.read_csv(verifications_by_rp_csv)
+    # ## Getting the volume of sessions for sign up and sign in
+    # No longer uses unique page views - gets the number of sessions for each value of the JOURNEY_TYPE custom variable
+    # rp_mapping translates the referrer url reported in the verifications csv
+    with open(f'{VERIFY_DATA_PIPELINE_CONFIG_PATH}/configuration/rp_mapping.json') as ft:
+        rp_mapping = json.load(ft)
 
-# Create export file with all RPs data
-df_export.to_csv(f'{report_output_path}/rp_report-{date_start_string}.csv')
+    def get_rp_name(rp_entity_id):
+        return rp_mapping[rp_entity_id]
 
-# Create export file per RP
+    # Get the human readable IDP name and add it to a new column
+    df_verifications_by_rp['rp'] = df_verifications_by_rp.apply(lambda row: get_rp_name(row['RP Entity Id']), axis=1)
+    df_verifications_by_rp.head()
+    df_verifications_by_rp = df_verifications_by_rp.rename(columns={'Response type': 'response_type'})
+    df_totals = df_verifications_by_rp.groupby(['rp', 'response_type']).count().reset_index()
+    df_totals.head()
+    df_totals.drop(['Timestamp', 'RP Entity Id'], axis=1, inplace=True)
+    df_totals = df_totals.rename(columns={'IDP Entity Id': 'successes'})
+    df_all = pd.pivot_table(df_totals, values='successes', index='rp', columns='response_type')
+    df_all.reset_index(inplace=True)
+    df_all.columns = ['rp', 'signup_success', 'signin_success']
+    # ### Success rate (sign in)
+    ls_rp = list(df_verifications_by_rp.rp.unique())
+    for rp in ls_rp:
+        print("Getting data for {}".format(rp))
+        segment_by_rp = f"customVariableValue1=={rp}"
+        all_referrals = get_nb_visits_for_rp(date_start_string, period, token, limit, segment_by_rp)
+        segment_signin = f"{segment_by_rp};customVariableValue3==SIGN_IN"
+        signin_attempts = get_nb_visits_for_rp(date_start_string, period, token, limit, segment_signin)
+        segment_signup = f"{segment_by_rp};customVariableValue3==REGISTRATION"
+        signup_attempts = get_nb_visits_for_rp(date_start_string, period, token, limit, segment_signup)
 
-for index, rp_data_row in df_export.iterrows():
-    rp_name = rp_data_row['rp']
-    rp_data_row.to_csv(f'{report_output_path}/rp_report-{date_start_string}-{rp_name}.csv')
+        segment_single_idp = f"{segment_by_rp};customVariableValue3==SINGLE_IDP"
+        single_idp_attempts = get_nb_visits_for_rp(date_start_string, period, token, limit, segment_single_idp)
+
+        # TODO if the RP is not found (e.g. due to no successful signins) then we may need to add a row?
+        df_all.loc[(df_all['rp'] == rp), 'all_referrals'] = all_referrals
+        df_all.loc[(df_all['rp'] == rp), 'signin_attempt'] = signin_attempts
+        df_all.loc[(df_all['rp'] == rp), 'signup_attempt'] = signup_attempts
+        df_all.loc[(df_all['rp'] == rp), 'single_idp_attempt'] = single_idp_attempts
+    df_all['signin_rate (deprecated)'] = df_all['signin_success'] / df_all['signin_attempt']
+    df_all['signup_rate (deprecated)'] = df_all['signup_success'] / df_all['signup_attempt']
+    df_all['all_referrals_with_intent'] = df_all['signin_attempt'] + df_all['signup_attempt'] + df_all[
+        'single_idp_attempt']
+    df_all['success'] = df_all['signin_success'] + df_all['signup_success']
+    df_all['success_rate'] = df_all['success'] / df_all['all_referrals_with_intent']
+    df_all['success_fraction_signup'] = df_all['signup_success'] / df_all['success']
+    df_all['success_fraction_signin'] = df_all['signin_success'] / df_all['success']
+    df_all['success_signup_rate'] = df_all['signup_success'] / df_all['all_referrals_with_intent']
+    df_all['success_signin_rate'] = df_all['signin_success'] / df_all['all_referrals_with_intent']
+    df_all['dropout'] = df_all['all_referrals_with_intent'] - df_all['signin_success'] - df_all['signup_success']
+    df_all['dropout_rate'] = df_all['dropout'] / df_all['all_referrals_with_intent']
+    # ### Ineligible (will not work / might not work) - with journey type segment
+    # upvson 'GOV.UK Verify will not work for you' page, segmented by 'registration' custom variable
+    for rp in ls_rp:
+        if is_loa2(rp):
+            will_not_work_page = "@GOV.UK Verify will not work for you - GOV.UK Verify - GOV.UK - LEVEL_2"
+            will_not_work_segment = "pageTitle={};customVariableValue1=={};customVariableValue3==REGISTRATION".format(
+                will_not_work_page, rp)
+            visits_will_not_work = get_nb_visits_for_page(date_start_string, period, token, limit,
+                                                          will_not_work_segment)
+            might_not_work_page = "@Why might this not work for me - GOV.UK Verify - GOV.UK - LEVEL_2"
+            might_not_work_segment = "pageTitle={};customVariableValue1=={};customVariableValue3==REGISTRATION".format(
+                might_not_work_page, rp)
+            visits_might_not_work = get_nb_visits_for_page(date_start_string, period, token, limit,
+                                                           might_not_work_segment)
+            df_all.loc[(df_all['rp'] == rp), 'visits_will_not_work'] = visits_will_not_work
+            df_all.loc[(df_all['rp'] == rp), 'visits_might_not_work'] = visits_might_not_work
+    # ## Export results
+    # Re-order columns and choose the ones we actually (currently) want in our report
+    df_export = df_all[
+        ['rp', 'all_referrals_with_intent', 'success', 'success_fraction_signup', 'success_fraction_signin',
+         'visits_will_not_work', 'visits_might_not_work']]
+    if not os.path.exists(report_output_path):
+        os.makedirs(report_output_path)
+    # Create export file with all RPs data
+    df_export.to_csv(f'{report_output_path}/rp_report-{date_start_string}.csv')
+    # Create export file per RP
+    for index, rp_data_row in df_export.iterrows():
+        rp_name = rp_data_row['rp']
+        rp_data_row.to_csv(f'{report_output_path}/rp_report-{date_start_string}-{rp_name}.csv')
+
+
+if __name__ == '__main__':
+    run()
