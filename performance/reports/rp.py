bad22d94c36f381de8d4663cf2d957283851e68c
George Lund <george.lund@digital.cabinet-office.gov.uk>
2018-09-28 10:32:44 +0100

    PMI-620: further comment the test upload function regarding config
     - explain why we need to use test configuration from non-test
       code, which sounds fairly bogus but is (somewhat) justified.
    
    https://trello.com/c/JDDrhFO3/620-reducing-the-manual-effort-of-generating-the-rp-mvp-reports

--- performance/reports/rp.py
+++ performance/reports/rp.py
@@ -68,6 +68,10 @@ def test_upload(gsheets_key, date_start):
     in this case we're actually going to do the upload the Google Sheets, this one is
     runnable independently from the command line.
     """
+    # Get test configuration because we need our rp_information to refer to the same set
+    # of test RP's as are used in the rp_report_weekly fixture. We're using the fixture
+    # because if you want to test the upload process, you don't want to have to wait around
+    # for Piwik - any data will do.
     config = performance.config.TestConfig()
     pygsheets_client = pygsheets.authorize(service_file=config.GSHEETS_CREDENTIALS_FILE)
     for rp_info in config.rp_information.values():
